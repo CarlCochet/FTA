b288b73daf019f68b13f4855bbd98265e2083a3c
CarlCochet <carl.cochet@gmail.com>
2019-09-02 17:29:39 +0200

    Added SFML/TGUI to ContentCreator

--- ContentCreator/CreatorWindow.cs
+++ ContentCreator/CreatorWindow.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 
+using TGUI;
+using SFML.System;
+
 namespace ContentCreator
 {
     class CreatorWindow
     {
-        
+        private SFML.Graphics.RenderWindow window;
+        private Gui gui;
+
 
         public void Run()
         {
